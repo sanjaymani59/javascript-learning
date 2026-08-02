fa78736646f001f21961f5a39bb5dfe2874face3
sanjay mani <sanjaymani9443@gmail.com>
2026-08-02 15:47:27 +0530

    javascript leap year code

---
+++ script.js
@@ -0,0 +1,19 @@
+let year =2004;
+
+if(year%4==0){
+    if(year%100 ==0){
+        if(year % 400==0){
+            console.log("it ia a leap year")
+        }
+        else{
+        console.log("not leap year")
+    }
+    }else{
+        console.log("leap year")
+    }
+
+}
+
+else{
+    console.log("it is not a leap year")
+}
