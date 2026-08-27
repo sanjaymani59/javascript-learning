d4c1e7e1972ed2042348ba43bd69be67199dd9cb
sanjay mani <sanjaymani9443@gmail.com>
2026-08-27 23:41:37 +0530

    java let var const

---
+++ hello.js
@@ -0,0 +1,27 @@
+/* let name ="sam"
+const age =39
+var city='oooo'
+
+console.log(name)
+console.log(age)
+console.log(city)
+
+
+const college='sammmmm'
+console.log(college)
+ */
+
+
+
+let a=19;
+const b=34;
+var c=30;
+
+
+a=34;
+b=34;
+c=300;
+
+console.log(a)
+console.log(b)
+console.log(c)
