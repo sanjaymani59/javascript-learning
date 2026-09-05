77f5ce91e36b0eea47b2eccd9777cc385da32d06
sanjay mani <sanjaymani9443@gmail.com>
2026-09-05 21:57:13 +0530

    reversed code

---
+++ pratice.js
@@ -0,0 +1,17 @@
+// loop concept
+
+// for (let i=0;i<=10;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+
+// reverse a string
+
+function rev(str){
+    return str.split("").reverse().join("");
+
+}
+
+console.log(rev("hello"));
