402efb301bdf67c09fc31c4ed620ded30353525b
sanjay mani <sanjaymani9443@gmail.com>
2026-09-07 23:05:18 +0530

    By youtube Todo list Project Creation using js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-let year =2004;
+// let year =2004;
 
-if(year%4==0){
-    if(year%100 ==0){
-        if(year % 400==0){
-            console.log("it ia a leap year")
-        }
-        else{
-        console.log("not leap year")
-    }
-    }else{
-        console.log("leap year")
-    }
+// if(year%4==0){
+//     if(year%100 ==0){
+//         if(year % 400==0){
+//             console.log("it ia a leap year")
+//         }
+//         else{
+//         console.log("not leap year")
+//     }
+//     }else{
+//         console.log("leap year")
+//     }
 
-}
+// }
 
-else{
-    console.log("it is not a leap year")
-}
+// else{
+//     console.log("it is not a leap year")
+// }
+
+
