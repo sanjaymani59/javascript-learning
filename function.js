5e0dca70b31e9d2b153b46d6fc4b4788ab75f503
sanjay mani <sanjaymani9443@gmail.com>
2026-09-23 23:37:39 +0530

    factorial

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-function EvenOdd(num){
+/* function EvenOdd(num){
     let even =0;
     let odd=0;
 
@@ -20,4 +20,23 @@ let num=[23,45,56,78,99];
 let result =EvenOdd(num)
 
 console.log(result.even);
-console.log(result.odd);
+console.log(result.odd);*/
+
+
+
+function fact(num){
+    let result=1;
+
+    for (let i=1;i<=num;i++){ // 1,2,3,4,5
+        result =result*i;     // resutl =1*1;  ==> result =1
+        // result =1*2;  ===>  result = 2
+        // result =2*3   ===>  RESULT = 6
+        // result =6*4   ===>  RESULT = 24
+        // result =24*5  ===>  RESULT = 120//
+
+
+
+    }
+    return result;
+}
+console.log(fact(5));
