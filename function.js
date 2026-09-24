1a156f9cea4963f77198d0d371ab3beb36c100e0
sanjay mani <sanjaymani9443@gmail.com>
2026-09-25 00:41:41 +0530

    javascript _ writing

--- function.js
+++ function.js
@@ -24,7 +24,7 @@ console.log(result.odd);*/
 
 
 
-function fact(num){
+/* function fact(num){
     let result=1;
 
     for (let i=1;i<=num;i++){ // 1,2,3,4,5
@@ -39,4 +39,67 @@ function fact(num){
     }
     return result;
 }
-console.log(fact(5));
+console.log(fact(5)); */
+
+
+// 8. Find Largest Number in an Array
+
+
+/* function Largestnum(num){
+
+    let large =num[0];
+
+
+    for (let i=1;i<num.length;i++){
+        if(num[i]>large){
+            large=num[i];
+       
+        }
+    }
+    return large;
+}
+
+let num=[23,45,22,90,23]
+console.log(Largestnum(num)) */
+
+
+// 
+// 9. Count Even Numbers
+
+/* function oddeven(num){
+    let count=0;
+    for (let i in num){
+        if (i %2!=0){
+            count++;
+        }
+    
+    }return count;
+
+}
+
+let num=[4,5,6,7,4,3,2]
+console.log(oddeven(num)) */
+
+
+// 10. Find Student with Highest Mark
+
+function findtop(Student){
+    let topmark = Student[0];
+    for (let i=1;i<Student.length;i++){
+        if (Student[i].mark > topmark.mark){
+            topmark =Student[i];
+        }
+    }
+    return topmark;
+
+}
+
+
+let Student=[{name:"sam",mark:98},
+            {name:"jone",mark:93},
+            {name:"riyas",mark:95}];
+
+let result=findtop(Student)
+
+console.log("name", result.name);
+console.log("Mark", result.mark)
